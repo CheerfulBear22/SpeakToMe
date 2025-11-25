925e15216d821f72a967587d3c5250516a907e58
CheerfulBear22 <197518179+CheerfulBear22@users.noreply.github.com>
2025-11-25 20:34:32 +0000

    Reduced the packet structure for greater simplicity

--- STM_PC/STM_PC.h
+++ STM_PC/STM_PC.h
@@ -1,13 +1,11 @@
-#ifndef STH_PC_H
+#ifndef STM_PC_H
 #define STM_PC_H
 
 #include <stdint.h>
 
 typedef struct {
-	uint8_t start_byte;
 	uint8_t message_type;
-	uint16_t payload_len;
-	uint16_t[256] payload;
-	uint16_t check;
+	uint8_t payload_len;
+	uint8_t[256] payload;
 } STM_Packet;
 #endif
