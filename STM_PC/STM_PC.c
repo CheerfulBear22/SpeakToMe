6d241aa1657e64f1c090ce5ca53c1b19ef50c6a5
CheerfulBear22 <197518179+CheerfulBear22@users.noreply.github.com>
2025-11-26 11:40:43 +0000

    Added a simple data sending function to STM_PC.c

--- STM_PC/STM_PC.c
+++ STM_PC/STM_PC.c
@@ -1,6 +1,7 @@
 #include "STM_PC.h"
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdint.h>
 
 STM_Packet deserialiseSTMPacket(uint8_t[] buff) {
 	STM_Packet packet = {0};
@@ -20,4 +21,9 @@ uint8_t[] serialiseSTM_Packet(STM_Packet* packet) {
 	memcpy(&buff[3], packet.payload, pkt.payload_len);
 
 	return buff;
+}
+
+int sendSTMMessage(int fd, uint8_t[] buff) {
+	if (write(fd, buff, sizeof(buff)) == -1)
+		return -1;
 }
