aff5a10a2fa883403dd051b75c5c1e5ebbe6bea0
CheerfulBear22 <197518179+CheerfulBear22@users.noreply.github.com>
2025-11-26 12:00:28 +0000

    Fixed array declarations

--- STM_PC/STM_PC.c
+++ STM_PC/STM_PC.c
@@ -9,7 +9,7 @@ To Do:
 - Memory security
 */
 
-STM_Packet deserialiseSTMPacket(uint8_t[] buff) {
+STM_Packet deserialiseSTMPacket(uint8_t* buff) {
 	STM_Packet packet = {0};
 
 	packet.message_type = buff[0];
@@ -19,8 +19,8 @@ STM_Packet deserialiseSTMPacket(uint8_t[] buff) {
 	return packet;
 }
 
-uint8_t[] serialiseSTM_Packet(STM_Packet* packet) {
-	uint8_t[sizeof(STM_Packet)] buff;
+uint8_t* serialiseSTM_Packet(STM_Packet* packet) {
+	uint8_t buff[sizeof(STM_Packet)];
 
 	buff[0] = packet.message_type;
 	buff[1] = packet.payload_len;
@@ -29,7 +29,7 @@ uint8_t[] serialiseSTM_Packet(STM_Packet* packet) {
 	return buff;
 }
 
-int sendSTMMessage(int fd, uint8_t[] buff) {
+int sendSTMMessage(int fd, uint8_t* buff) {
 	if (write(fd, buff, sizeof(buff)) == -1)
 		return -1;
 }
--- STM_PC/STM_PC.h
+++ STM_PC/STM_PC.h
@@ -6,11 +6,11 @@
 typedef struct {
 	uint8_t message_type;
 	uint8_t payload_len;
-	uint8_t[256] payload;
+	uint8_t payload[256];
 } STM_Packet;
 
-STM_Packet deserialiseSTM_Packet(uint8_t[] buff);
-uint8_t[] serialiseSTM_Packet(STM_Packet* packet);
+STM_Packet deserialiseSTM_Packet(uint8_t* buff);
+uint8_t* serialiseSTM_Packet(STM_Packet* packet);
 
-void sendSTMMessage(int fd, uint8_t[] buff);
+void sendSTMMessage(int fd, uint8_t* buff);
 #endif
